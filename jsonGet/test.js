9e820d85c0d05e7ec46b99b73d19b00a47543a0b
sorajiao <sorajiao@tencent.com>
2020-04-27 14:50:41 +0800

    更新一波递归不报错的代码，还没搞定

--- jsonGet/test.js
+++ jsonGet/test.js
@@ -1,68 +1,51 @@
-const raw = require('./rawJSON.json');
+const raw = require('./test.json');
 
 const body = raw.program.body
 
 const TransferRaw = function (body) {
-  let result ={};
   
-  body.map(item => {
-    let suites = [];
+  let result ={};
+  let suites = [];
+
+  body.map((item,index) => {  
     let res = {};
+    let suites = [];
     let title = item.expression.arguments[0] || '';
-    let fnBody = item.expression.arguments[1];
-    let desObj = item.expression.arguments[2];
+    let fnBody, desObj;
+
+    //第三个参数存在证明第二个参数（函数体）存在
+    if(item.expression.arguments[2]) {
+      fnBody = item.expression.arguments[1];
+      desObj = item.expression.arguments[2];
+    }else {
+      desObj = item.expression.arguments[1];
+    }
+
+    // console.log(fnBody);
 
     title = title.value;
     res['title'] = title;
 
+    //遍历标志对象，添加进res
     if(desObj) {
       desObj.properties.map(item => {
         res[item.key.name] = item.value.value;
       })
     }
-    
     suites.push(res);
 
-    result['suites'] = suites;
+    if(fnBody) {
+      console.log(suites)
+      TransferRaw(fnBody.body.body)
+    }
     
   })
 
-
+  result['suites'] = suites
 
   return result;
   
   
-  
-  
-  // const fnBody2 = fnBody.body.body;
-
-  // const suites2 = fnBody2.map(item => {
-  //   let suites = [];
-  //   let res = {};
-  //   let title = item.expression.arguments[0];
-  //   let fnBody = item.expression.arguments[1];
-  //   let desObj = item.expression.arguments[2];
-
-  //   title = title.value;
-  //   res['title'] = title;
-
-  //   if(desObj) {
-  //     desObj.properties.map(item => {
-  //       res[item.key.name] = item.value.value;
-  //     })
-  //   }
-    
-  //   suites.push(res);
-    
-  //   return suites;
-  // })
-  
-  // res['suites'] = suites2;
-
-  // suites.push(res);
-
-  // return suites;
-  
 }
-
+// TransferRaw(body)
 console.log(JSON.stringify(TransferRaw(body), null, 2));
